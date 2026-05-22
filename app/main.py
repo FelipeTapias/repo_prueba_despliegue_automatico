993c79247bf8ef707a40391a17c09e9cadc6180d
Andrés Felipe Tapias <62154763+FelipeTapias@users.noreply.github.com>
2026-05-22 17:41:56 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def saludo(nombre: str):
 @app.get("/loteria/{numero}")
 def saludo(numero: str):
         if("123" == numero):
-            return { "mensaje:": "Eres el feliz ganador"}
+            return { "mensaje:": "Eres el feliz ganador..."}
         else:
-             return { "mensaje:": "Eres el triste perdedor"}
+             return { "mensaje:": "Eres el triste perdedor..."}
     
